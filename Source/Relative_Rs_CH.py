ad71f79eb4cfb90be3902e26fd29d8f0502d1cc7
samiramali <samiramali@users.noreply.github.com>
2025-04-08 21:32:12 -0400

    Update Relative_Rs_CH.py

--- Source/Relative_Rs_CH.py
+++ Source/Relative_Rs_CH.py
@@ -12,10 +12,10 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import csv
 
-# Define the filename
+# Output file
 filename = "Rs_data_Tolokh_final_UpConversionPlusRelative.csv"
 
-#data = pd.read_csv('/Users/samir/Desktop/lamin_mutant_new.csv')
+# Input file
 data = pd.read_csv('/Data/3rd_set.csv')
 
    
